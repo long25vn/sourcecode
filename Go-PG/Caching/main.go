cdd6f2841655120b17eba2d7f42be9233e8cede3
Seventy Nine <31624384+long25vn@users.noreply.github.com>
2019-06-17 16:44:24 +0700

    Create main.go
    Create main.go

---
+++ Go-PG/Caching/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-pg/pg"
+	"github.com/kataras/iris"
+
+	"github.com/kataras/iris/cache"
+)
+
+
+func main() {
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Get("/", cache.Handler(20*time.Second), routeDemo) // Cache in 20 minutes
+
+	app.Run(iris.Addr(":8080"))
+}
+
+func routeDemo(ctx iris.Context) {
+	fmt.Println("Handler executed. Content refreshed.")
+
+	db := Connect("postgres", "123456", "postgres", "localhost:5432")
+	defer db.Close()
+
+	var book int32
+	_, err := db.Query(&book, `
+		SELECT discount_value
+		FROM promotion.voucher WHERE id = 'bk3hcovbuiv1e2tpgvt0'
+	`)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(book)
+	ctx.JSON(book)
+}
+
+func Connect(user, password, database, addr string) (db *pg.DB) {
+	db = pg.Connect(&pg.Options{
+		User:     user,
+		Password: password,
+		Database: database,
+		Addr:     addr,
+	})
+	var n int
+	_, err := db.QueryOne(pg.Scan(&n), "SELECT 1")
+	if err != nil {
+		panic(err)
+	}
+	if n == 1 {
+		fmt.Println("Connected")
+	}
+	return db
+}
